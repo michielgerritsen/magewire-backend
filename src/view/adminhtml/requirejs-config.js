3c5677ae89f482dc48d986ff8fef53fa9ebf1fbc
Jisse Reitsma <jisse@yireo.com>
2023-12-11 19:55:49 +0100

    Rewrite of magewire-message dep on customer-data in frontend

---
+++ src/view/adminhtml/requirejs-config.js
@@ -0,0 +1,6 @@
+var config = {
+    paths: {
+        Livewire: 'Magewirephp_Magewire/js/livewire',
+        'Magewirephp_MagewireRequireJs/js/magewire-message': 'Magewirephp_MagewireBackend/js/magewire-message'
+    }
+}
